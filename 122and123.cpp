9e132ead6d8dcd9b77cc06d1cf6ad9f7448f1533
Mjx <12773018+Jack_mj14@user.noreply.gitee.com>
2024-04-18 22:28:30 +0800

    2

--- 122and123.cpp
+++ 122and123.cpp
@@ -15,6 +15,7 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n, vector<int>(5, 0));
         dp[0][1] = -prices[0];
+        dp[0][3] = -prices[0];
         for (int i = 1; i < n; i++) {
             dp[i][0] = dp[i - 1][0];
             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
