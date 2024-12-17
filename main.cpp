4c14a66d0fc0937348b54a909627c47d10bb38db
Mjx <12773018+Jack_mj14@user.noreply.gitee.com>
2024-12-18 00:48:15 +0800

    4

--- main.cpp
+++ main.cpp
@@ -6,26 +6,39 @@
 
 using namespace std;
 
-int main() {
-    string x;
-    getline(cin, x);
+class Solution {
+private:
+    void traversal(TreeNode *cur, string path,
 
-    stringstream ss(x);
-
-    vector<int> v;
-
-    int num;
-
-    while (getline(ss, x, ',')) {
-        num = stoi(x);
-        v.push_back(num);
-    }
-    sort(v.begin(), v.end());
-
-    for (int i = 0; i < v.size(); i++) {
-        if (i != 0) {
-            cout << ' ';
+    *
+    vector<string> &result
+    ) {
+        path += to_string(cur->val); // 中，中为什么写在这里，因为最后一个节点也要加入到path中
+        if (cur->left == NULL && cur->right == NULL) {
+            result.push_back(path);
+            return;
         }
-        cout << v[i];
+        if (cur->left) {
+            path += "->";
+            traversal(cur->left, path, result); // 左
+            path.pop_back(); // 回溯 '>'
+            path.pop_back(); // 回溯 '-'
+        }
+        if (cur->right) {
+            path += "->";
+            traversal(cur->right, path, result); // 右
+            path.pop_back(); // 回溯'>'
+            path.pop_back(); // 回溯 '-'
+        }
     }
-}
+
+public:
+    vector<string> binaryTreePaths(TreeNode *root) {
+        vector<string> result;
+        string path;
+        if (root == NULL) return result;
+        traversal(root, path, *result);
+        return result;
+
+    }
+};
