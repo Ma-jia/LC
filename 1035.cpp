43cf76e6bad46010d15bb6110b77aa2e7819daa2
Mjx <12773018+Jack_mj14@user.noreply.gitee.com>
2025-02-19 22:46:53 +0800

    6

---
+++ 1035.cpp
@@ -0,0 +1,29 @@
+//
+// Created by m'j'x on 2025/2/19.
+//
+#include <iostream>
+#include<sstream>
+#include<vector>
+#include<algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <stdio.h>
+#include <stdbool.h>
+
+class Solution {
+public:
+    int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2) {
+        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
+        for (int i = 1; i <= nums1.size(); ++i) {
+            for (int j = 1; j <= nums2.size(); ++j) {
+                if (nums1[i - 1] == nums2[j - 1])dp[i][j] = dp[i - 1][j - 1] + 1;
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[nums1.size()][nums2.size()];
+    }
+};
